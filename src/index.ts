c5a3c7a6ccdc27651721dbce55afe2c82769c3a8
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2026-01-01 17:45:52 -0600

    fix(practs): bump to latest best (#18)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { given, when, then } from './givenWhenThen';
-export { genTestUuid } from './genTestUuid';
-export { usePrep, useBeforeAll, useBeforeEach } from './usePrep';
-
 // forward the getError method since it is almost always needed with tests
 export { getError } from 'helpful-errors';
+
+export { genTestUuid } from './genTestUuid';
+export { given, then, when } from './givenWhenThen';
+export { useBeforeAll, useBeforeEach, usePrep } from './usePrep';
